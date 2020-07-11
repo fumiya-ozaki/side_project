2bc0f32b46209b224dd30b9e80578f181d1df59f
fumiya-ozaki <fumiya0414@gmail.com>
2020-07-11 09:28:03 +0900

    adjust header.php
    ヘッダーのタイトルを修正

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>side project</title>
+    <title><?php echo wp_get_document_title();?></title>
     <?php wp_head(); ?><!--headerfookpoint-->
   </head>
   <body <?php body_class(); ?>> <!--bodystartpoint-->
